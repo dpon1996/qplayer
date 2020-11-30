52107e4468d28ac1dbc8b4f628910add138c675d
= <=>
2020-11-30 13:03:11 +0530

    test

--- lib/qplayer.dart
+++ lib/qplayer.dart
@@ -1,7 +1,15 @@
 library qplayer;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+class QPlayer{
+  Widget qPlayer({msg = "data"}){
+    return Text(msg);
+  }
 }
