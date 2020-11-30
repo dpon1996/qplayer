3b96f371f296520f59d274bbe96da42153d10cbb
= <=>
2020-11-30 19:04:43 +0530

    player selector

--- lib/playerStyles/basicPlayerStyle.dart
+++ lib/playerStyles/basicPlayerStyle.dart
@@ -14,59 +14,66 @@ class _BasicPlayerStyleState extends State<BasicPlayerStyle> {
   bool visibility = false;
   PlayerProvider _playerProvider;
   PlayerFunctionsProvider _playerFunctionsProvider;
+
   @override
   Widget build(BuildContext context) {
     _playerProvider = Provider.of<PlayerProvider>(context);
     _playerFunctionsProvider = Provider.of<PlayerFunctionsProvider>(context);
     return InkWell(
-      onTap: (){
+      onTap: () {
         _playerFunctionsProvider.getVideoProgressValue();
         setState(() {
           visibility = !visibility;
         });
-        if(visibility){
-          Timer(Duration(seconds: 2), (){
+        if (visibility) {
+          Timer(Duration(seconds: 2), () {
             setState(() {
               visibility = false;
             });
           });
         }
       },
-      child: Visibility(
-        visible: visibility,
-        child: Container(
-          color: Colors.black12,
+      child: Container(
           alignment: Alignment.center,
-          child: Stack(
-            children: [
-              Center(
-                child: IconButton(
-                  onPressed: (){
-                    if(_playerProvider.videoPlayerController.value.isPlaying){
-                      _playerProvider.videoPlayerController.pause();
-                    }else{
-                      _playerProvider.videoPlayerController.play();
-                    }
-                  },
-                  icon: Icon(Icons.play_circle_outline,size: 30,color: Colors.white,),
-                ),
+          child: Visibility(
+            visible: visibility,
+            child: Container(
+              color: Colors.black12,
+              child: Stack(
+                children: [
+                  Center(
+                    child: IconButton(
+                      onPressed: () {
+                        if (_playerProvider
+                            .videoPlayerController.value.isPlaying) {
+                          _playerProvider.videoPlayerController.pause();
+                        } else {
+                          _playerProvider.videoPlayerController.play();
+                        }
+                      },
+                      icon: Icon(
+                        Icons.play_circle_outline,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    left: 0,
+                    right: 0,
+                    child: Column(
+                      children: [
+                        LinearProgressIndicator(
+                          value: _getVideoPosition(),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Positioned(
-                bottom: 20,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: [
-                    LinearProgressIndicator(
-                      value: _getVideoPosition(),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          )
-        ),
-      ),
+            ),
+          )),
     );
   }
 
